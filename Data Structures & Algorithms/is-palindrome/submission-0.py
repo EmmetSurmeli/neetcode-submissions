283f915114d26a5a9469afebc061eceb7765147d
Emmet Surmeli <emmetsuphi@icloud.com>
2026-06-24 16:24:29 -0400

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -3,13 +3,13 @@ class Solution:
         i = 0
         j = len(s) - 1
         while i < j:
-            if (s[i].isalnum() and s[j].isalnum()):
+            if s[i] == ' ' or not s[i].isalnum():
+                i += 1
+            elif s[j] == ' ' or not s[j].isalnum():
+                j -= 1
+            else:
                 if s[i].lower() != s[j].lower():
                     return False
                 i += 1
                 j -= 1
-            elif s[i].isalnum():
-                j -= 1
-            else:
-                i += 1
         return True
