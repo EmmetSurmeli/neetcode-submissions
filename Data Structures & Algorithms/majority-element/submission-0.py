affa9b1c3323c141511f56f09170aac14514533b
Emmet Surmeli <emmetsuphi@icloud.com>
2026-06-29 11:53:55 -0400

    Update: majority-element - submission-0

--- Data Structures & Algorithms/majority-element/submission-0.py
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -1,10 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        myDict = {}
-        for i in nums:
-            if i in myDict:
-                myDict[i] += 1
-            else:
-                myDict[i] = 1
-            if myDict[i] > len(nums)//2:
-                return i
+        nums.sort()
+        return nums[len(nums) // 2]
