cbce52377ca3205764ce3cb812be89add8f9d29a
Emmet Surmeli <emmetsuphi@icloud.com>
2026-07-01 13:31:07 -0400

    Add: find-words-that-can-be-formed-by-characters - submission-0

---
+++ Data Structures & Algorithms/find-words-that-can-be-formed-by-characters/submission-0.py
@@ -0,0 +1,18 @@
+from collections import Counter
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        #BRUTE FORCE
+        total = 0
+        characters = Counter(chars)
+        temp = Counter(chars)
+        for i in words:
+            for j in range(len(i)):
+                if i[j] in temp and temp[i[j]] > 0:
+                    temp[i[j]] -=1
+                else:
+                    break
+                if j == len(i) - 1:
+                    total += len(i)
+            temp = Counter(chars)
+
+        return total
