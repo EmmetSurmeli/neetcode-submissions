057adcf311d600c8729cc72638f5094cc0905793
Emmet Surmeli <emmetsuphi@icloud.com>
2026-07-08 15:31:01 -0400

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,30 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        diction = {}
-        tempList = []
-
-        for word in range(0,len(strs)): #sorting each word
-            tempList.append("".join(sorted(strs[word])))
-
-        #getting all the different anagram letters
-        mySet = set()
-        for n in tempList:
-            mySet.add(n)
-        
-        #creating dictionary with the keys of the different letters, empty values
-        keys = []
-        for x in mySet:
-            keys.append(x)
-        diction = {key: [] for key in keys} 
-
-        #adding each word in strs to appropriate list in dict
-        for x in mySet:
-            for i in range(0, len(tempList)):
-                if x == tempList[i]:
-                    diction[x].append(strs[i])
-
-        #creating and outputting final list
-        finalList = []
-        for j in diction:
-            finalList.append(diction[j])
-        return finalList
+        myDict = {}
+        for i in strs:
+            arr = [0] * 26
+            for j in i:
+                arr[ord(j) - ord('a')] += 1
+            tupe = tuple(arr)
+            if tupe in myDict:
+                myDict[tupe].append(i)
+            else:
+                myDict[tupe] = [i]
+        return list(myDict.values())
