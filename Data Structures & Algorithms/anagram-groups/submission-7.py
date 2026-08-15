6a9409108b8dc577e257218633d2b2167fdc6dcc
Emmet Surmeli <ess256@cornell.edu>
2026-08-14 23:41:08 -0400

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        myDict = {}
+        for word in strs:
+            arr = [0] * 26
+            for letter in word:
+                arr[ord(letter) - ord('a')] += 1
+            if tuple(arr) in myDict:
+                myDict[tuple(arr)].append(word)
+            else:
+                myDict[tuple(arr)] = [word]
+        output = []
+        for i in myDict.values():
+            output.append(list(i))
+        return output
